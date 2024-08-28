d58ba7d3e6cb95d7bdb31d11f801688cddffb3ac
DESKTOP-HHV3M9S\wontae <wontae.gramin9@gmail.com>
2024-08-28 14:22:32 +0900

    [LocalStorage] Darkmode 구현

---
+++ 12_WebStorage/localStorage/darkModeApp/app.js
@@ -0,0 +1,28 @@
+const toggleButton = document.querySelector("#toggleMode");
+
+const toogleTheme = () => {
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "light");
+    document.body.classList.remove("dark-mode");
+    toggleButton.textContent = "Enable dark mode";
+  } else {
+    localStorage.setItem("theme", "dark");
+    document.body.classList.add("dark-mode");
+    toggleButton.textContent = "Enable light mode";
+  }
+};
+
+toggleButton.addEventListener("click", () => {
+  toogleTheme(); // toggle되고, localStorage에 toggle값을 넣는것까지 완성
+});
+
+// 새로고침/처음 접근했을 때 기존의 theme을 localStorage에서 가져와야 함
+const applySavedTheme = () => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    toggleButton.textContent = "Enable light mode";
+  }
+};
+
+applySavedTheme();
